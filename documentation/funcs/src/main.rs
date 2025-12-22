f311d66c76264f355925e23d7200c0f65d4390db
Bharat <bsha0529@uni.sydney.edu.au>
2025-12-22 21:42:34 +1100

    rustlings exercises commit 1

---
+++ documentation/funcs/src/main.rs
@@ -0,0 +1,10 @@
+fn five () -> i32 {
+    7
+}
+
+
+fn main(){
+    let x =  five();
+
+    println!("the val of x is = {x}");
+}
---
+++ documentation/funcs/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/bspsdn/rust_projects/learning/funcs/target/debug/hello_cargo: /home/bspsdn/rust_projects/learning/funcs/src/main.rs
